723056cb315f406755c0b0a7c9064e97ae1448f5
AgileIntelligence <io.agileintelligence@gmail.com>
2019-10-20 12:30:32 -0400

    first commit

---
+++ fsbasics1-backend/src/test/java/io/agileintelligence/fsbasics1backend/Fsbasics1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.fsbasics1backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fsbasics1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
