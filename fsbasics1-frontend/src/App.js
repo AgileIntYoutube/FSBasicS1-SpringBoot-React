e4951f90a350765fe5d6c09761c6d4791c810447
AgileIntelligence <io.agileintelligence@gmail.com>
2019-10-28 13:49:30 -0400

    EP16-JSX-InitialComp-Planning

--- fsbasics1-frontend/src/App.js
+++ fsbasics1-frontend/src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 function App() {
+  const title = "Project Task Frontend From JS const";
+  const allGood = true;
+
+  // const test = () => {
+  //   if (allGood) {
+  //     return "confirmed all good";
+  //   }
+  // };
+
   return (
-    <div className="App">
-      <h1>Project Task Frontend</h1>
-    </div>
+    <Fragment>
+      <div className="App">
+        <h1>{title}</h1>
+        <label htmlFor=""></label>
+        <input tabIndex="-1"></input>
+        <p>All good </p>
+        {allGood ? <p>"ternary"</p> : <p>"false"</p>}
+        <div>
+          <p> not cool</p>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
